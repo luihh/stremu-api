38426d62625058d9f873e4aa1ce7e5fa27721de0
Luihh <luihh@proton.me>
2025-08-07 20:41:48 -0500

    improve /api/stream/

--- src/routes/stream/index.ts
+++ src/routes/stream/index.ts
@@ -1,39 +1,52 @@
-import express, { Router } from 'express';
+import express from 'express';
 import fs from 'node:fs';
+import { Song } from '../../models/index.js';
+
+import type { Router, Request, Response } from 'express';
 
 const router: Router = express.Router();
 
-router.get('/:filename', (req, res) => {
-  const fileName = req.params.filename;
-  const { pathname: filePath } = new URL(
-    `../../../songs/${fileName}`,
-    import.meta.url
-  );
+router.get('/:id', async (req: Request, res: Response) => {
+  const songId = Number(req.params.id);
+  if (isNaN(songId)) return res.status(400).json({ error: 'Invalid song ID' });
 
-  fs.stat(filePath, (err, stats) => {
-    if (err) {
-      return res.status(404).send('File not found.');
-    }
+  try {
+    const song = await Song.findByPk(songId);
+    if (!song)
+      return res.status(404).json({ error: 'Song not found in database' });
 
-    const CHUNK_SIZE = 500 * 1e3; // 0.5MB
+    const { pathname: filePath } = new URL(
+      `../../../songs/${songId}.mp3`,
+      import.meta.url
+    );
 
-    const range = req.headers.range || '0';
-    const start = Number(range.replace(/\D/g, ''));
-    const end = Math.min(start + CHUNK_SIZE, stats.size - 1);
-    const contentLength = end - start + 1;
+    fs.stat(filePath, (err, stats) => {
+      if (err) {
+        return res.status(404).send('File not found.');
+      }
 
-    const headers = {
-      'Content-Range': `bytes ${start}-${end}/${stats.size}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': contentLength,
-      'Content-Type': 'audio/mpeg',
-      'Transfer-Encoding': 'chunked',
-    };
+      const CHUNK_SIZE = 500 * 1e3; // 0.5MB
 
-    res.writeHead(206, headers);
-    const stream = fs.createReadStream(filePath, { start, end });
-    stream.pipe(res);
-  });
+      const range = req.headers.range || '0';
+      const start = Number(range.replace(/\D/g, ''));
+      const end = Math.min(start + CHUNK_SIZE, stats.size - 1);
+      const contentLength = end - start + 1;
+
+      const headers = {
+        'Content-Range': `bytes ${start}-${end}/${stats.size}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': contentLength,
+        'Content-Type': 'audio/mpeg',
+        'Transfer-Encoding': 'chunked',
+      };
+
+      res.writeHead(206, headers);
+      const stream = fs.createReadStream(filePath, { start, end });
+      stream.pipe(res);
+    });
+  } catch (err) {
+    return res.status(500).json({ error: err });
+  }
 });
 
 export default router;
